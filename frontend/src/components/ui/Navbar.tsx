6d56ce3d347df8ae177e3aa7514963aa005bfb29
Bubblie01 <manrohesh@gmail.com>
2025-04-15 01:32:09 -0400

    navbar mostly working

--- frontend/src/components/ui/Navbar.tsx
+++ frontend/src/components/ui/Navbar.tsx
@@ -1,15 +1,31 @@
 import { Button } from './button'
-
+import pawLogo from '../../assets/paw_logo.png';
 const navItems = ['Login'];
 
 export default function MainNav() {
     return (
-      <div className="mr-4 hidden gap-2 md:flex justify-end">
-        {navItems.map((item, index) => (
-          <Button key={index} variant="link">
-            {item}
-          </Button>
-        ))}
+      <div className = "w-full">
+        <div className="mx-auto flex items-center justify-between px-4 sm:px-6 lg:px-8">
+        {/* Logo + Title */}
+        <div className="flex items-center gap-2">
+          <img 
+            className="rounded-full w-20 h-20" 
+            src={pawLogo} 
+            alt="paw logo" 
+          />
+          <h1 className="text-xl font-bold">MeowMatch</h1>
+        </div>
+
+        {/* Nav items on the right */}
+        <div className="flex gap-2">
+          {navItems.map((item, index) => (
+            <Button key={index} variant="link">
+              {item}
+            </Button>
+          ))}
+        </div>
       </div>
+      <hr className="border-b mt-2 border-gray-200 -mx-8 sm:-mx-6 lg:-mx-8"/>
+    </div>
     );
   }
