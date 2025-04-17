5d07407be911fc496197b37b0a3ced0b48b27fa3
Bubblie01 <manrohesh@gmail.com>
2025-04-17 01:36:19 -0400

    Joe sucks (homepage is basically done)

--- frontend/src/components/ui/Navbar.tsx
+++ frontend/src/components/ui/Navbar.tsx
@@ -12,7 +12,7 @@ export default function MainNav() {
       <div className = "w-full">
         <div className="mx-auto flex items-center justify-between px-4 sm:px-6 lg:px-8">
         {/* Logo + Title */}
-        <div className="flex items-center gap-2">
+        <div className="flex items-center text-center gap-10">
           <img 
             className="rounded-full w-20 h-20" 
             src={pawLogo} 
@@ -24,7 +24,7 @@ export default function MainNav() {
         {/* Nav items on the right */}
         <div className="flex gap-2 ">
           {navItems.map((item) => (
-            <Button asChild variant = "secondary" key = {item.path}>
+            <Button asChild variant = "ghost" key = {item.path}>
               <Link to = {item.path}>
                 {item.name}
               </Link>
--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -1,11 +1,11 @@
 import catPng from '@/assets/cat1.png'
-
+import { Button } from "@/components/ui/button"
 
 function HomePage() {
     return(
         <>
-        <div className = "flex flex-row">
-        <div className = "flex flex-col">
+        <div className = "flex flex-row my-52">
+        <div className = "flex flex-col gap-10 text-center">
             <h1 className="text-left mb-4 text-4xl">Find your purrfect match</h1>
             <p className = "text-left mb-4">
             Every cat deserves a cozy home and a loving companion. 
@@ -13,6 +13,14 @@ function HomePage() {
             we're here to help you find your purrfect match. 
             Start your adoption journey today and give a cat the forever home they’ve been waiting for. 🐾
             </p>
+            <div className = "flex flex-row gap-15">
+                <Button className = "w-58 h-15 gap-2">
+                    Browse Cats
+                </Button>
+                <Button className = "w-58 h-15 gap-2">
+                    Post Cats
+                </Button>
+            </div>
         </div>
         <img className = "w-80 h-80" src={catPng}/>
         </div>
