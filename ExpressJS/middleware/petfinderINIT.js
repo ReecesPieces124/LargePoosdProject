367527a50799caeecc0feb386864e469fb240426
Joseph Peters <petersjoseph19@gmail.com>
2025-04-19 18:57:08 -0400

    Update petfinderINIT.js
    Converted to CommonJS to be consistent with rest of API

--- ExpressJS/middleware/petfinderINIT.js
+++ ExpressJS/middleware/petfinderINIT.js
@@ -1,27 +1,26 @@
-// Init for the petfinder API in order to get everything loaded in.
-import axios from "axios";
+// this file allows the petfinder token to be created for use within our API at numerous parts
+const axios = require("axios");
 
-// token information
-let token, tokenExpiration = 0;
+let token;
+let tokenExpiration = 0;
 
-// getToken should be called before any other API calls to ensure a valid token is used
-// function establishes a token in order to use petfinder API !
-export async function getToken() {
-    // give token if it hasn't expired
-    if (Date.now() < tokenExpiration) return token;
+// allows us to receive the token from petfinder API and store it in a variable
+async function getTokenPF() {
+  if (Date.now() < tokenExpiration) return token;
 
-    const { data } = await axios.post(
-    'https://api.petfinder.com/v2/oauth2/token',
+  const { data } = await axios.post(
+    "https://api.petfinder.com/v2/oauth2/token",
     new URLSearchParams({
-        grant_type: 'client_credentials',
-        client_id: 'OUR_PF_KEY_ENV',
-        client_secret: 'OUR_PF_SECRET_ENV'
+      grant_type: "client_credentials",
+      client_id: process.env.OUR_PF_KEY_ENV,
+      client_secret: process.env.OUR_PF_SECRET_ENV
     })
-    );
+  );
 
-    // receives a token and sets expiration time
-    token = data.access_token;
-    tokenExpiration = Date.now() + 3600 * 1000; // 1 hour until expiration
-    // returns new token
-    return token;
+  token = data.access_token;
+  // set the token expiration to 1 hour from now
+  tokenExpiration = Date.now() + 3600 * 1000;
+  return token;
 }
+
+module.exports = { getTokenPF };
