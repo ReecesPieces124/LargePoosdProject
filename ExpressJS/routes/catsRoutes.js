987b14fdc2449def647174bc67fb413460d1d8ef
Joseph Peters <jpeters39@valenciacollege.edu>
2025-04-22 16:36:11 -0400

    typo

--- ExpressJS/routes/catsRoutes.js
+++ ExpressJS/routes/catsRoutes.js
@@ -4,13 +4,13 @@ const express = require("express");
 const router = express.Router();
 const {
   getCats,
-  getCatByPfID,
+  getCatByID,
   deleteCat,
   createCat // included for optional use (admin/manual testing)
 } = require("../controllers/catsController");
 
 router.get("/", getCats);               // GET all cats
-router.get("/pfid/:pfID", getCatByPfID); // GET cat by pfID
+router.get("/pfid/:pfID", getCatByID); // GET cat by pfID
 router.post("/", createCat);            // POST new cat (fallback / test route)
 router.delete("/:id", deleteCat);       // DELETE by _id
 
